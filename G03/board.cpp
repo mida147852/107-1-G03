d64d2e99cca679fe6c039c2cf31fc63ef11d19c1
fuchieh0614 <43245417+fuchieh0614@users.noreply.github.com>
2019-01-04 18:19:57 +0800

    Update board.cpp

--- G03/board.cpp
+++ G03/board.cpp
@@ -1,21 +1,20 @@
-#include "g03.h"
+#include "G03.h"
 
-void printBoard(int board[4][4]) {
-
-    for(int i=0;i<4;i++){
-        printf("|");
-        for(int j=0;j<4;j++){
-            if(board[i][j]==0) printf("    0");
-            else printf("%5d",board[i][j]);
-          }
-        printf("|\n\n");
+void Ar(int k[4][4]){
+    for(int i=0; i<4; i++)
+    {
+        printf("---------------------\n|");
+        for(int j=0; j<4; j++)
+        {
+            printf("%4d|",k[i][j]);
+        }
+        printf("\n");
     }
-
+    printf("---------------------\n");
 }
 
-void readBoard(int board[4][4]) {
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++)
-        scanf("%d",&board[i][j]);
-    }
+void matrix(int k[4][4],int moves){
+    printf("Right(d),Left(a),On(w),Under(s),Save(r),Exit(e)\n");
+    Ar(k);
+    printf("move= %d \n",moves);
 }
